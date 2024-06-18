20b8a98a6d9afd4cd5f4b1fcc7c92d9759c7e186
Brad Owens <bowens@machinesense.com>
2024-06-18 09:33:57 -0500

    Adderd the script that does the squashfs overlay fix.

--- target/linux/bcm27xx/image/gen_rpi_sdcard_img.sh
+++ target/linux/bcm27xx/image/gen_rpi_sdcard_img.sh
@@ -23,6 +23,26 @@ set $(ptgen -o $OUTPUT -h $head -s $sect -l $align -t $kernel_type -p ${BOOTFSSI
 
 BOOTOFFSET="$(($1 / 512))"
 ROOTFSOFFSET="$(($3 / 512))"
+#ROOTFSSIZE="$(($4 / 512))"
+
+ROOTFSSIZE_BYTES="$(($ROOTFSSIZE * 1024 * 1024 * 1024))"
+
+ROOTFSBYTES="$(wc -c < $ROOTFS)"
+ROOTFSIMGSIZE="$(( ($ROOTFSBYTES+511) / 512))"
+PADOFFSET="$((ROOTFSOFFSET + $ROOTFSIMGSIZE - 1))"
+PADSIZE="$((ROOTFSSIZE_BYTES/512 - ROOTFSIMGSIZE + 1 ))"
+
+#echo "PADSIZE: " $PADSIZE
+
+if [ "$PADSIZE" -gt 2048 ]; then
+	PADSIZE=2048
+fi
+
+#echo "FINAL PADSIZE: " $PADSIZE
 
 dd bs=512 if="$BOOTFS" of="$OUTPUT" seek="$BOOTOFFSET" conv=notrunc
+
+[ $PADSIZE -gt 0 ] && dd bs=512 if=/dev/zero of="$OUTPUT" seek="$PADOFFSET" count="$PADSIZE" conv=notrunc
+
+
 dd bs=512 if="$ROOTFS" of="$OUTPUT" seek="$ROOTFSOFFSET" conv=notrunc
